0f51e7dbbea71f0e484e0bdc9a7204e3ddc68f2a
Kyudyoz <kyudyozz84@gmail.com>
2023-05-26 22:56:09 +0700

    update table

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,8 +4,8 @@
      <h1 class="text-center">Postingan {{ auth()->user()->name }}</h1>
 </div>
 
-<div class="table-responsive pb-0" >
-    <table class="table table-striped table-sm text-white table-dark">
+<div class="table-responsive">
+    <table class="table table-striped-columns table-sm table-dark">
       <thead class="text-center">
         <tr>
           <th scope="col">No</th>
